52510b1f1e7b8bce7804a58b3ddd983c0f986668
Garrett Grube <gdgrube@gmail.com>
2024-03-26 10:47:20 -0400

    root updates

---
+++ app/constants.ts
@@ -0,0 +1,4 @@
+export const GITHUB_REPOSITORY_URL = "https://github.com/gdg10/my-site";
+export const LINKEDIN_PROFILE_URL = "https://www.linkedin.com/in/garrett-grube/";
+export const RESUME_PDF_PATH = "/GarrettGrube_Resume_2024.03.26.pdf";
+export const HEADSHOT_PATH = "/headshot.png";
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,29 +1,36 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Flex, Text, Button, Heading, Avatar, Card, Box, Container, Link, Code } from '@radix-ui/themes';
-import { ArrowRightIcon, DownloadIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
-import { useNavigate } from "@remix-run/react";
+import { Flex, Text, Button, Heading, Avatar, Box, Container } from '@radix-ui/themes';
+import { DownloadIcon, GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { GITHUB_REPOSITORY_URL, HEADSHOT_PATH, LINKEDIN_PROFILE_URL, RESUME_PDF_PATH } from "~/constants";
+import { motion } from "framer-motion";
 
 export const meta: MetaFunction = () => [{ title: "Garrett Grube" }];
 
 export default function Index() {
-  const navigate = useNavigate();
   return (
-    <Box className="h-screen">
-      <Container size="1">
-        <Flex direction="column" gap="6" align="center" py='8' px="4">
-          <Avatar fallback="GG" size="9" />
-          <Heading align='center'>Garrett Grube</Heading>
-          <Text align='center'>Senior Full-Stack Engineer</Text>
-          {/* <Flex gap='6' align="center"> */}
-          <Button><DownloadIcon /> Resume Download</Button>
-          <Button><GitHubLogoIcon /> This site on Github</Button>
-          <Button><LinkedInLogoIcon /> Linkedin Profile</Button>
-          {/* </Flex> */}
-          {/* <Button size='4' onClick={() => navigate('/engineer')}>Software Engineer <ArrowRightIcon /></Button> */}
-          {/* TODO: <Button size='4' onClick={() => navigate('/drums')}>Drummer <ArrowRightIcon /></Button>          */}
-          {/* <Link size='2' target='_blank' href="https://github.com/gdg10" className="mt-auto m-auto text-center">I hand-coded this website using React, Remix, & Radix UI. See the source on Github.</Link> */}
-        </Flex>
-      </Container>
+    <Box className="h-screen bg-[var(--accent-1)]">
+      <motion.div
+        initial={{ rotate: 0, scaleY: 0 }}
+        animate={{ rotate: 0, scaleY: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+      >
+        <Container size="1">
+          <Flex direction="column" gap="3" align="center" justify='between' py='8' px="4">
+            <Flex direction="column" align='center' gap="1" mb="3">
+              <Avatar src={HEADSHOT_PATH} fallback="GG" size="9" className="w-full rounded"/>
+              <Heading size='9' align='center'>Garrett Grube</Heading>
+              <Text size='5' align='center'>Senior Full-Stack Engineer, Scrum Master</Text>
+            </Flex>
+            <Button size='4' onClick={() => window.open(RESUME_PDF_PATH)} className="w-full"><DownloadIcon /> Resume download</Button>
+            <Button size='4' onClick={() => window.open(LINKEDIN_PROFILE_URL)} className="w-full"><LinkedInLogoIcon /> Linkedin profile</Button>
+            <Button size='4' onClick={() => window.open(GITHUB_REPOSITORY_URL)} className="w-full"><GitHubLogoIcon /> This website's source code</Button>
+          </Flex>
+        </Container>
+      </motion.div>
     </Box>
   );
 }
