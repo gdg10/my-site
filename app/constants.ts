4db99501525de7c89f91df1d61578e425edc18d1
Garrett Grube <gdgrube@gmail.com>
2024-12-08 11:19:03 -0500

    chore: update resume

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 export const GITHUB_REPOSITORY_URL = "https://github.com/gdg10/my-site";
 export const LINKEDIN_PROFILE_URL =
   "https://www.linkedin.com/in/garrett-grube/";
-export const RESUME_PDF_PATH = "/GarrettGrube_Resume_2024.03.26.pdf";
+export const RESUME_PDF_PATH = "/GarrettGrube_Resume.pdf";
 export const HEADSHOT_PATH = "/headshot.png";
